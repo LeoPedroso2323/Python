da26e0c777182a94350a36209c1c1f1b4532ec0e
LeoPedroso2323 <159189231+LeoPedroso2323@users.noreply.github.com>
2024-03-25 15:17:40 -0300

    Exercícios 1 e 2 - OP Arit. e PAR

---
+++ exercicio1.py
@@ -0,0 +1,14 @@
+import time
+
+numero1 = 10
+numero2 = 15
+
+print("Adição:", numero1 + numero2)
+print("Subtração:", numero1 - numero2)
+print("Multiplicação:", numero1 * numero2)
+print("Divisão:", numero1 / numero2)
+print("\n\nDivisão Inteira:", numero1 // numero2)
+print("\n\nResto da Divisão:", numero1 % numero2)
+print("\n\nPotência:", numero1 ** numero2)
+
+time.sleep(5)
---
+++ literal.py
@@ -0,0 +1,46 @@
+import time
+
+print("Soma de inteiros:" , (123 + 12) * 10)
+#Convertendo para octal
+print("Octal:" , oct(42))
+#Convertendo para hexadecimal
+print("Hexadecimal:", hex(42))
+#Convertendo para binario
+print("Binario:", bin(42))
+
+print("floats")
+print(0.4)
+print(4.0)
+print(4)
+print(4.0 + 4)
+
+print(4.)
+print(.4)
+
+print(3E8)
+print(3e8)
+
+#6,62667 x 10 (-34) (elevado a -34)
+print(6.62607e-34)
+
+#Representação da notação 0.000000000000000000001
+print(0.000000000000000000001)
+
+print("Strings")
+
+print('Brasil "1 x 0" Inglaterra')
+
+
+print("Boolean ou Bool")
+
+print(True)
+print(False)
+
+print("Comparação 1:", True > False)
+print("Comparação 2:", True < False)
+
+
+
+
+
+time.sleep(5)
